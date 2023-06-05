5e84570b956ab61e6a9dddbe000f52b9c1fb4aa3
dupont <dupontcam@gmail.com>
2023-06-05 16:31:56 -0300

    "Payment"

--- src/main/java/com/devsuperior/DSCommerce/entities/Order.java
+++ src/main/java/com/devsuperior/DSCommerce/entities/Order.java
@@ -10,21 +10,28 @@ public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant moment;
+
     private OrderStatus status;
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
 
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
+
     public Order() {
     }
 
-    public Order(Long id, Instant moment, OrderStatus status, User client) {
+    public Order(Long id, Instant moment, OrderStatus status, User client, Payment payment) {
         this.id = id;
         this.moment = moment;
         this.status = status;
         this.client = client;
+        this.payment = payment;
     }
 
     public Long getId() {
@@ -53,5 +60,17 @@ public class Order {
 
     public User getClient() {
         return client;
+    }
+
+    public void setClient(User client) {
+        this.client = client;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
     }
 }
