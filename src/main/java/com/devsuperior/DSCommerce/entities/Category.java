5213cf3bb0b1adab51e2c89674d05382fb695e35
dupont <dupontcam@gmail.com>
2023-06-05 17:25:43 -0300

    "Category and Product"

---
+++ src/main/java/com/devsuperior/DSCommerce/entities/Category.java
@@ -0,0 +1,53 @@
+package com.devsuperior.DSCommerce.entities;
+
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "tb_category")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "categories")
+    private Set<Produt> products = new HashSet<>();
+
+    public Category() {
+    }
+    public Category(Long id, String name, Set<Produt> products) {
+        this.id = id;
+        this.name = name;
+        this.products = products;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Set<Produt> getProducts() {
+        return products;
+    }
+}
