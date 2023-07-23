9eb09b5660295d6345dc62a7fa95c4ae4d977981
dupontcam <125046483+dupontcam@users.noreply.github.com>
2023-07-23 17:09:27 -0300

    Saving Order

--- src/main/java/com/devsuperior/DSCommerce/services/OrderService.java
+++ src/main/java/com/devsuperior/DSCommerce/services/OrderService.java
@@ -1,12 +1,21 @@
 package com.devsuperior.DSCommerce.services;
 
+import java.time.Instant;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.DSCommerce.dto.OrderDTO;
+import com.devsuperior.DSCommerce.dto.OrderItemDTO;
 import com.devsuperior.DSCommerce.entities.Order;
+import com.devsuperior.DSCommerce.entities.OrderItem;
+import com.devsuperior.DSCommerce.entities.OrderStatus;
+import com.devsuperior.DSCommerce.entities.Product;
+import com.devsuperior.DSCommerce.entities.User;
+import com.devsuperior.DSCommerce.repositories.OrderItemRepository;
 import com.devsuperior.DSCommerce.repositories.OrderRepository;
+import com.devsuperior.DSCommerce.repositories.ProductRepository;
 import com.devsuperior.DSCommerce.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -14,6 +23,15 @@ public class OrderService {
 	
 	@Autowired
 	private OrderRepository repository;
+	
+	@Autowired
+	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
+	
+	@Autowired
+	private UserService userService;
 
 	@Transactional(readOnly = true)
     public OrderDTO finById(Long id){
@@ -21,4 +39,26 @@ public class OrderService {
                 () -> new ResourceNotFoundException("Recurso não encontrado"));
         return new OrderDTO(order);
     }
+
+	@Transactional
+	public OrderDTO insert(OrderDTO dto) {
+		
+		Order order = new Order();
+		order.setMoment(Instant.now());
+		order.setStatus(OrderStatus.WAITING_PAYMENT);
+		
+		User user = userService.authenticated();
+		order.setClient(user);
+		
+		for(OrderItemDTO itemDTO : dto.getItems()) {
+			Product product = productRepository.getReferenceById(itemDTO.getProductId());
+			OrderItem item = new OrderItem(order, product, itemDTO.getQuantity(), product.getPrice());
+			order.getItems().add(item);
+		}
+		
+		repository.save(order);
+		orderItemRepository.saveAll(order.getItems());
+		
+		return new OrderDTO(order);		
+	}
 }
